0fcf7c96fb4826ba4eef4305ddb49de33442bbf6
Olivier Laviale <olivier.laviale@gmail.com>
2015-08-23 01:33:09 +0200

    Prototype config fragments are now their own file

--- config/prototype.php
+++ config/prototype.php
@@ -14,15 +14,8 @@ namespace Icybee\Modules\Cache;
 use ICanBoogie;
 use Icybee;
 
-$hooks = Hooks::class . '::';
-
 return [
 
-	'events' => [
-
-		Icybee\Modules\Modules\ActivateOperation::class . '::process' => $hooks . 'on_modules_change',
-		Icybee\Modules\Modules\DeactivateOperation::class . '::process' => $hooks . 'on_modules_change'
-
-	]
+	ICanBoogie\Core::class . '::get_caches' => CacheCollection::class . '::get'
 
 ];
